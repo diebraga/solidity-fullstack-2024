947e9a3526f56b17eb102e5060974953a42eef4c
Diego Braga <diebraga0055@gmail.com>
2024-03-14 11:47:10 +0000

    hawrdhat setup

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,5 +1,8 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
 
 contract SimpleStorage {
     uint256 favoriteNumber;
